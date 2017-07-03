88d489954a42141e4499b174f41f9cae2356f3b6
Ivan Honchar <ivan.honchar@globallogic.com>
2017-07-03 17:00:08 +0300

    fix(TCOMP-595): set env variables for hadoop winutils in start.bat

--- services/components-api-service-rest/scripts/start.bat
+++ services/components-api-service-rest/scripts/start.bat
@@ -6,4 +6,8 @@ SET APP_CLASS="org.talend.components.service.rest.Application"
 set THISDIR=%~dp0
 SET CLASSPATH=.\config;.\config\default;${project.artifactId}-${project.version}.jar
 
+REM Set env variables which points to hadoop winutils binaries. It is required for S3 component
+SET HADOOP_HOME=%THISDIR%binaries\hadoop
+SET PATH=%PATH%;%HADOOP_HOME%\bin
+
 java %JAVA_OPTS% -Xmx2048m -Dfile.encoding=UTF-8 -Dorg.ops4j.pax.url.mvn.localRepository="%THISDIR%\.m2" -Dcomponent.default.config.folder="%THISDIR%\config\default" -cp %CLASSPATH% %APP_CLASS%
